689d5b86a2397cbd0b88391581e1e788b147aea1
Sigvald Marholm <sigvald.marholm@ife.no>
2024-10-03 13:45:43 +0200

    Added light colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+-- Custom file by SM
+
+return {
+
+  -- Add coloschemes from git
+  { "sainnhe/edge" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "EdenEast/nightfox.nvim" },
+
+  -- Set default
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dayfox",
+    },
+  },
+}
